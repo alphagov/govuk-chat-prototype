7aef729edfaa74800bc87772778cd843892536d6
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-07-28 15:58:17 +0100

    Remove redcarpet and markdown helper - we are assuming an HTML response

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def markdown(text)
-    # The options work with plain text. They are being ommited whilst we test rendering...
-    # ...the API response, which includes HTML.
-    # options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks]
-    Markdown.new(text).to_html.html_safe
-  end
 end
