2c0f4a30e082b238a0db738098e876d2cda1eff8
James Chan <james.chan@digital.cabinet-office.gov.uk>
2023-08-06 01:18:29 +0100

    Fix JS to scroll to latest message

--- app/javascript/govuk-chat.js
+++ app/javascript/govuk-chat.js
@@ -1,15 +1,50 @@
-console.log("GOVUK Chat JS loaded")
+(function initialiseGOVUKChatJS() {
+    // Used for debugging purposes
+    console.log("GOVUK Chat JS loaded");
 
-if(document.querySelector(".govuk-chat-timestamp")) {
-    var chatContainer = document.querySelector(".govuk-chat-container");
-    var latestMessage = document.querySelectorAll(".govuk-chat-timestamp")[(document.querySelectorAll(".govuk-chat-timestamp").length - 1)]
+    var hasReceivedNewMessage = initialiseMessageState();
 
-    scrollToLatestMessage({
-        chatContainer: chatContainer,
-        latestMessage:latestMessage
-    })
-}
+    // Set up mutation observer
+    var target = document.querySelector("html");
+    var config = { childList: true }
+    var observer = new MutationObserver(function() {
+        if(document.querySelector(".govuk-chat-timestamp")) {
+            var chatContainer = document.querySelector(".govuk-chat-container");
+            var messageCount = document.querySelectorAll(".govuk-chat-timestamp").length;
+            var latestMessage = document.querySelectorAll(".govuk-chat-timestamp")[(messageCount - 1)];
+            var newMessageReceived = hasReceivedNewMessage(messageCount);
+
+            scrollToLatestMessage({
+                chatContainer: chatContainer,
+                latestMessage:latestMessage,
+                newMessageReceived: newMessageReceived
+            })
+        }
+    });
+
+    observer.observe(target, config);
+})();
 
 function scrollToLatestMessage(params) {
-    params.chatContainer.scrollTop = params.latestMessage.getBoundingClientRect().y - 60;
+    if(params.latestMessage.getBoundingClientRect().y - 60 > 0 && params.newMessageReceived) {
+        params.chatContainer.scrollTop = params.latestMessage.getBoundingClientRect().y - 60;
+    }
+}
+
+function initialiseMessageState() {
+    var messageCount = 0;
+
+    return function (messages) {
+        var newMessageReceived = false;
+
+        if(!messages) {
+            messageCount = 0;
+        }
+        else if(messages > messageCount) {
+            newMessageReceived = true;
+            messageCount = messages;
+        }
+
+        return newMessageReceived
+    }
 }
