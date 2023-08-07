ef02be52ba1a8a59a4f74b05c2f319d306dcd704
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2023-08-07 15:03:23 +0100

    Create alerts table
    Create an alerts table which will store a chat id, this balue will be updated
    as part of redteaming.
    
    This is a temprorary feature and will be removed after redteaming.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_080314) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_135634) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "hstore"
   enable_extension "plpgsql"
+
+  create_table "alerts", force: :cascade do |t|
+    t.string "chat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "chats", force: :cascade do |t|
     t.string "chat_id"
