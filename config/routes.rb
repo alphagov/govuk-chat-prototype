994f727445d12282fcb185794d970cb9dc9d45ac
Rosa Fox <rosafox89@gmail.com>
2023-10-02 16:55:44 +0100

    Add Passwordless routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  passwordless_for :users, at: '/', as: :auth
   get "chats/refresh", as: "refresh"
   get "chats/onboarding", as: "onboarding"
   get "feedbacks/complete", as: "complete"
