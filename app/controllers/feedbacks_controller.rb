191f346f4d9fd23bc7360080a01df74864ff2c5e
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-10-03 13:11:44 +0100

    Add conversation level feedback - Add datatype partials - Add hint to feedback questions

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,10 +1,11 @@
 class FeedbacksController < ApplicationController
   def new
-    @feedback = Feedback.new(uuid: params[:uuid])
+    @feedback = Feedback.new(level: "conversation", uuid: params[:uuid], version: ENV["CONVERSATION_FEEDBACK_VERSION"])
   end
 
   def create
     @feedback = Feedback.new(feedback_params)
+    @feedback.response = params["answers"].to_json
 
     if @feedback.save
       redirect_to new_chat_url(uuid: @feedback.uuid), notice: "Thanks for your feedback."
@@ -14,6 +15,6 @@ class FeedbacksController < ApplicationController
 private
 
   def feedback_params
-    params.require(:feedback).permit(:uuid, :rating, :content)
+    params.require(:feedback).permit(:chat_id, :uuid, :version, :level, :response)
   end
 end
