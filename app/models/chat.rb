e3157a467ba4a22a25c418d1eecfad9e8d84f046
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-10-03 09:45:04 +0100

    Add env vars for feedback config - Add feeback yaml files - Associate Chat and Feedback models - Load feedback yaml files based on env var config

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,6 @@
 class Chat < ApplicationRecord
+  has_many :feedbacks
+
   def answer
     JSON.parse(reply)["answer"]
   end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,26 @@
 class Feedback < ApplicationRecord
+  belongs_to :chat, optional: true
+
+  def self.message_questions
+    @@message_questions ||= load_questions("message")
+  end
+
+  def message_questions
+    self.class.message_questions
+  end
+
+  def self.conversation_questions
+    @@conversation_questions ||= load_questions("conversation")
+  end
+
+  def conversation_questions
+    self.class.conversation_questions
+  end
+
+private
+
+  def self.load_questions(level)
+    config = YAML.load_file("feedback/#{level}.yaml")
+    config[ENV["#{level.upcase}_FEEDBACK_VERSION"]]
+  end
 end
