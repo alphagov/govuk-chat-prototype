2d14ddc74cd567ae7d72805339a6e9640b5477e7
Rosa Fox <rosafox89@gmail.com>
2023-10-04 09:18:18 +0100

    Rake task to create magic link data (Passwordless::Sessions) for users
    The code for creating the token was copied over from the Gem.

--- lib/tasks/magic_link.rake
+++ lib/tasks/magic_link.rake
@@ -1,9 +1,16 @@
 require_relative 'populate_users'
+require_relative 'populate_passwordless_sessions'
 namespace :magic_link do
   #Individual tasks for creating users and sending emails
 
   #Create new users
   task :create_users, [:emails] => [:environment] do |t, args|
     PopulateUsers.create(args.emails)
+  end
+
+  #Create magic links for users
+  #Users must have already been created using create_users
+  task :create_magic_links, [:emails] => [:environment] do |t, args|
+    PopulatePasswordlessSessions.create(args.emails)
   end
 end
---
+++ lib/tasks/populate_passwordless_sessions.rb
@@ -0,0 +1,44 @@
+class PopulatePasswordlessSessions
+  def self.create(emails)
+    emails.split(" ").each do |email|
+      user = User.find_by_email(email)
+      #Help to prevent multiple passwordless sessions per user
+      unless Passwordless::Session.find_by_authenticatable_id(user.id)
+        self.create_passwordless_session(user)
+      end
+    end
+  end
+
+private
+
+  def self.create_passwordless_session(user)
+    Passwordless::Session.create(
+          authenticatable_type: "User",
+          authenticatable_id: user.id,
+          user_agent: "na",
+          remote_addr: "na",
+          token: create_token
+        )
+    puts "Created session for: #{user.email}"
+  end
+
+  def self.create_token
+    OpenSSL::HMAC.hexdigest(algorithm, key, str)
+  end
+
+  def self.algorithm
+    "SHA256"
+  end
+
+  def self.chars
+    chars = [*"A".."Z", *"0".."9"].freeze
+  end
+
+  def self.str
+    chars.sample(6).join
+  end
+
+  def self.key
+    ActiveSupport::KeyGenerator.new(Rails.application.secret_key_base).generate_key("passwordless")
+  end
+end
