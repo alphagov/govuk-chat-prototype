4062dde44e40d7f2f9bd019ccee208921a21f53f
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-10-11 16:11:39 +0100

    Update models with CSV export methods

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,18 @@
 class Chat < ApplicationRecord
   has_many :feedbacks
 
+  scope :for_csv_export, -> {
+    select(self.headers).where(uuid: Feedback.for_csv_export.map(&:uuid))
+  }
+
+  def created_at_formatted
+    created_at.strftime("%d/%m/%Y %H:%M:%S")
+  end
+
+  def self.headers
+    [:id, :uuid, :prompt, :reply, :created_at]
+  end
+
   def answer
     begin
       JSON.parse(reply)["answer"]
