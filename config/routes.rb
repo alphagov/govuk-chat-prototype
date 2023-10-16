b4c9f4b36b4218d59dcb8fd48589d731904d1d76
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2023-10-16 11:57:47 +0100

    Add outro page for the feedback form
    This commit adds a new page to thank users who choose to leave us feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "chats/refresh", as: "refresh"
   get "chats/onboarding", as: "onboarding"
+  get "feedbacks/complete", as: "complete"
   resources :feedbacks
   resources :chats
   root "chats#index"
