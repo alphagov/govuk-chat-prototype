44e8281f2254dc2a98efea576662fe7c29961680
Rosa Fox <rosafox89@gmail.com>
2023-10-18 17:47:55 +0100

    Regenerate schema after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_145304) do
 ActiveRecord::Schema[7.0].define(version: 2023_10_03_153827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
