c93d7135fe88c12016f7c174ffc4a097ccbbc3d5
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-11-29 08:43:36 +0000

    Add job tests

--- test/jobs/background_api_call_job_test.rb
+++ test/jobs/background_api_call_job_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class BackgroundApiCallJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "enqueues a new job" do
+    assert_enqueued_with(job: BackgroundApiCallJob) do
+      BackgroundApiCallJob.perform_later("12345678-1234-1234-1234-123456789ABC", "How are you?", 1)
+    end
+    assert_equal 2, Chat.count
+  end
 end
